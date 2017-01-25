4bb861ef0177640e2247774f213ffd2b7fe5c409
Adrien Plazas <kekun.plazas@laposte.net>
2017-01-25 17:47:02 +0100

    retro-gobject: Fix style of core memory functions
    Fix the style of retro_core_get_memory() and retro_core_set_memory() to
    make it closer to the GLib C style.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=777489

--- retro-gobject/retro-core.c
+++ retro-gobject/retro-core.c
@@ -2,43 +2,45 @@
 
 #include "retro-gobject-internal.h"
 
-guint8* retro_core_get_memory (RetroCore* self, RetroMemoryType id, int* length) {
+guint8 *retro_core_get_memory (RetroCore *self, RetroMemoryType id, gint *length) {
+	RetroGetMemoryData get_mem_data;
+	RetroGetMemorySize get_mem_size;
+	guint8 *data;
+	gsize size;
+
 	g_return_val_if_fail (self != NULL, NULL);
 	g_return_val_if_fail (length != NULL, NULL);
 
+	get_mem_data = retro_module_get_get_memory_data (self->module);
+	get_mem_size = retro_module_get_get_memory_size (self->module);
+
 	retro_core_push_cb_data (self);
-
-	RetroModule* module = self->module;
-
-	RetroGetMemoryData get_mem_data = retro_module_get_get_memory_data (module);
-	RetroGetMemorySize get_mem_size = retro_module_get_get_memory_size (module);
-
-	guint8* data = get_mem_data (id);
-	gsize size = get_mem_size (id);
-
+	data = get_mem_data (id);
+	size = get_mem_size (id);
 	retro_core_pop_cb_data ();
 
 	data = g_memdup (data, size);
-	*length = (gint) (data ? size : 0);
+	*length = (gint) (data != NULL ? size : 0);
 
 	return data;
 }
 
-void retro_core_set_memory (RetroCore* self, RetroMemoryType id, guint8* data, int length) {
+void retro_core_set_memory (RetroCore *self, RetroMemoryType id, guint8 *data, gint length) {
+	RetroGetMemoryData get_mem_region;
+	RetroGetMemorySize get_mem_region_size;
+	guint8 *memory_region;
+	gsize memory_region_size;
+
 	g_return_if_fail (self != NULL);
 	g_return_if_fail (data != NULL);
 	g_return_if_fail (length > 0);
 
+	get_mem_region = retro_module_get_get_memory_data (self->module);
+	get_mem_region_size = retro_module_get_get_memory_size (self->module);
+
 	retro_core_push_cb_data (self);
-
-	RetroModule* module = self->module;
-
-	RetroGetMemoryData get_mem_region = retro_module_get_get_memory_data (module);
-	RetroGetMemorySize get_mem_region_size = retro_module_get_get_memory_size (module);
-
-	guint8* memory_region = get_mem_region (id);
-	gsize memory_region_size = get_mem_region_size (id);
-
+	memory_region = get_mem_region (id);
+	memory_region_size = get_mem_region_size (id);
 	retro_core_pop_cb_data ();
 
 	g_return_if_fail (memory_region != NULL);

716558625d87336ca063a57ee6f4a38915d9b332
Bastien Nocera <hadess@hadess.net>
2017-02-16 08:07:28 +0100

    retro-gtk: Add script to update libretro.h
    Based of update-from-gsd.sh, itself based off update-from-egg.sh, but
    with white space trailing removal and srcdir != builddir support
    added in.

---
+++ retro-gtk/update-from-retroarch.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+function die() {
+  echo $*
+  exit 1
+}
+
+if test -z "$DIR"; then
+   echo "Must set DIR"
+   exit 1
+fi
+
+if test -z "$SRCDIR"; then
+   echo "Must set SRCDIR"
+   exit 1
+fi
+
+if test -z "$FILES"; then
+   echo "Must set FILES"
+   exit 1
+fi
+
+cd "$SRCDIR"
+
+for FILE in $FILES; do
+  TMPFILE=`mktemp`
+  sed 's/[ \t]*$//' "$DIR/$FILE" > $TMPFILE
+  if cmp -s $TMPFILE $FILE; then
+     echo "File $FILE is unchanged"
+  else
+     cp $TMPFILE $FILE || die "Could not move $DIR/$FILE to $FILE"
+     echo "Updated $FILE"
+     git add $FILE
+  fi
+done

744ea6881a6dba3767857245b87d6fbb6abc25c0
Adrien Plazas <kekun.plazas@laposte.net>
2017-09-25 08:24:40 +0200

    core: Fix a memory leaks in destructor

--- retro-gtk/retro-core.c
+++ retro-gtk/retro-core.c
@@ -695,6 +695,9 @@ retro_core_destructor (RetroCore *self)
   if (internal->media_uris != NULL)
     g_strfreev (internal->media_uris);
 
+  g_object_unref (internal->module);
+  g_object_unref (internal->options);
+
   g_free (self->environment_internal);
 }
 

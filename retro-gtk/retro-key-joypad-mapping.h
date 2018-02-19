0b5a7da6269fa67a4aeeb9e52a5738d41858412c
theawless <theawless@gmail.com>
2018-02-19 18:09:55 +0530

    Add RetroKeyJoypadMapping
    This is required to provide configuration for keyboard.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=780755

---
+++ retro-gtk/retro-key-joypad-mapping.h
@@ -0,0 +1,29 @@
+// This file is part of retro-gtk. License: GPL-3.0+.
+
+#ifndef RETRO_KEY_JOYPAD_MAPPING_H
+#define RETRO_KEY_JOYPAD_MAPPING_H
+
+#if !defined(__RETRO_GTK_INSIDE__) && !defined(RETRO_GTK_COMPILATION)
+# error "Only <retro-gtk.h> can be included directly."
+#endif
+
+#include <glib-object.h>
+#include "retro-controller-codes.h"
+
+G_BEGIN_DECLS
+
+#define RETRO_TYPE_KEY_JOYPAD_MAPPING (retro_key_joypad_mapping_get_type())
+
+G_DECLARE_FINAL_TYPE (RetroKeyJoypadMapping, retro_key_joypad_mapping, RETRO, KEY_JOYPAD_MAPPING, GObject)
+
+void retro_key_joypad_mapping_set_button_key (RetroKeyJoypadMapping *self,
+                                              RetroJoypadId          button,
+                                              guint16                key);
+guint16 retro_key_joypad_mapping_get_button_key (RetroKeyJoypadMapping *self,
+                                                 RetroJoypadId          button);
+void retro_key_joypad_mapping_set_to_defaults (RetroKeyJoypadMapping *self);
+RetroKeyJoypadMapping *retro_key_joypad_mapping_new ();
+
+G_END_DECLS
+
+#endif /* RETRO_KEY_JOYPAD_MAPPING_H */

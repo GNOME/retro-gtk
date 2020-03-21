7e9f2238a599daa0d6b6ca9685879c75b39582ad
Alexander Mikhaylenko <alexm@gnome.org>
2020-03-21 18:10:38 +0500

    Introduce RetroProcAddress
    For now this is only going to be used in RetroHWRenderCallback, but
    libretro uses it in more places, so keep it separate.

---
+++ retro-runner/retro-proc-address-private.h
@@ -0,0 +1,13 @@
+// This file is part of retro-gtk. License: GPL-3.0+.
+
+#pragma once
+
+#if !defined(__RETRO_GTK_INSIDE__) && !defined(RETRO_GTK_COMPILATION)
+# error "Only <retro-gtk.h> can be included directly."
+#endif
+
+G_BEGIN_DECLS
+
+typedef void (*RetroProcAddress) (void);
+
+G_END_DECLS

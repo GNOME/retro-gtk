b8e17caaff8b1eff6564af182b2d73eded06fb0d
Adrien Plazas <kekun.plazas@laposte.net>
2020-11-13 00:17:16 +0100

    update-from-retroarch: Fix the indentation

--- tests/update-from-retroarch.sh
+++ tests/update-from-retroarch.sh
@@ -6,18 +6,18 @@ function die() {
 }
 
 if test -z "$DIR"; then
-   echo "Must set DIR"
-   exit 1
+  echo "Must set DIR"
+  exit 1
 fi
 
 if test -z "$SRCDIR"; then
-   echo "Must set SRCDIR"
-   exit 1
+  echo "Must set SRCDIR"
+  exit 1
 fi
 
 if test -z "$FILES"; then
-   echo "Must set FILES"
-   exit 1
+  echo "Must set FILES"
+  exit 1
 fi
 
 cd "$SRCDIR"
@@ -26,10 +26,10 @@ for FILE in $FILES; do
   TMPFILE=`mktemp`
   sed 's/[ \t]*$//' "$DIR/$FILE" > $TMPFILE
   if cmp -s $TMPFILE $FILE; then
-     echo "File $FILE is unchanged"
+    echo "File $FILE is unchanged"
   else
-     cp $TMPFILE $FILE || die "Could not move $DIR/$FILE to $FILE"
-     echo "Updated $FILE"
-     git add $FILE
+    cp $TMPFILE $FILE || die "Could not move $DIR/$FILE to $FILE"
+    echo "Updated $FILE"
+    git add $FILE
   fi
 done
